c5324b17db077a0475c58a0a74721ba48ac29d48
alammcajnp <alamjnp12@gmail.com>
2024-08-25 12:31:23 +0530

    post api

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/hello", (req, res) =>
   res.send("Hello Alamdeen Siddiqui This is my testing")
 );
 
+app.post("/helalamlo", (req, res) => {
+  const body = req.body;
+
+  let name = `Hello Mr/Ms ${body.name} Your Welcome, Doest not matter Today what is, Matter is that What are you doing today for build yor tomorrow`;
+
+  res.send(name);
+});
+
 // Start the server (useful for local testing)
 app.listen(3000, () => {
   console.log("App listening on port 3000");
