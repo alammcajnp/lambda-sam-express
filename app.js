8f9f2df16bebc96948e679b7df94cef56213ae28
alammcajnp <alamjnp12@gmail.com>
2024-08-25 12:26:09 +0530

    My changes

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
-app.get("/hello", (res, res) => res.send("Hello Alamdeen Siddiqui"));
+
+// Define a route handler with unique parameter names
+app.get("/hello", (req, res) => res.send("Hello Alamdeen Siddiqui"));
+
+// Start the server (useful for local testing)
 app.listen(3000, () => {
-  console.log("App listning port of 3000");
+  console.log("App listening on port 3000");
 });
+
+// Export the Lambda handler
 module.exports.handler = serverless(app);
