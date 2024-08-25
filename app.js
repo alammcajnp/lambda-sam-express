a842b1156f821b638e77c5efcc7e59a27b620576
alammcajnp <alamjnp12@gmail.com>
2024-08-25 12:38:02 +0530

    post api

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
 
+app.use(express.json());
+
 // Define a route handler with unique parameter names
 app.get("/hello", (req, res) =>
   res.send("Hello Alamdeen Siddiqui This is my testing")
 );
 
-app.post("/helalamlo", (req, res) => {
+app.post("/alam", (req, res) => {
   const body = JSON.stringify(req.body);
-
   let name = `Hello Mr/Ms ${body.name} Your Welcome, Doest not matter Today what is, Matter is that What are you doing today for build yor tomorrow`;
 
   res.send(name);
 });
 
 app.post("/data", (req, res) => {
-  const requestData = req.body;
-  console.log("Received data:", requestData);
-  res.send(`Received data: ${JSON.stringify(requestData)}`);
+  const name = req.body.name; // Access the `name` field from the JSON body
+  console.log("Received name:", name);
+  res.send(`Received name: ${name}`);
 });
 
 // Start the server (useful for local testing)
